04ba30a1473c8e63b5ab07270e3ec113348ba0f7
Andrey Zhereshchin <a.zhereshchin@gmail.com>
2016-09-06 23:17:53 +0100

    Typo in remote source

--- cookbooks/unity3d/recipes/default.rb
+++ cookbooks/unity3d/recipes/default.rb
@@ -8,17 +8,17 @@
 #
 remote_file 'UnityEditor' do
   path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:editor][:filename]}"
-  source "#{[:unity3d][:installer][:editor][:source]}"
+  source "#{node[:unity3d][:installer][:editor][:source]}"
 end
 
 remote_file 'StandardAssets' do
   path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:assets][:filename]}"
-  source "#{[:unity3d][:installer][:assets][:source]}"
+  source "#{node[:unity3d][:installer][:assets][:source]}"
 end
 
 remote_file 'SampleProject' do
   path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:sample][:filename]}"
-  source "#{[:unity3d][:installer][:sample][:source]}"
+  source "#{node[:unity3d][:installer][:sample][:source]}"
 end
 
 package 'UnityEditor' do
