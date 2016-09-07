20881e888512940dde632ef8fc282d2a129a66cb
Andrey Zhereshchin <a.zhereshchin@gmail.com>
2016-09-07 19:31:58 +0100

    Add check if already installed

--- cookbooks/unity3d/recipes/default.rb
+++ cookbooks/unity3d/recipes/default.rb
@@ -6,35 +6,44 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-remote_file 'UnityEditor' do
-  path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:editor][:filename]}"
-  source "#{node[:unity3d][:installer][:editor][:source]}"
-end
 
-remote_file 'StandardAssets' do
-  path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:assets][:filename]}"
-  source "#{node[:unity3d][:installer][:assets][:source]}"
-end
+unless node[:unity3d][:is_installed] do
+  remote_file 'UnityEditor' do
+    path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:editor][:filename]}"
+    source "#{node[:unity3d][:installer][:editor][:source]}"
+  end
 
-remote_file 'SampleProject' do
-  path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:sample][:filename]}"
-  source "#{node[:unity3d][:installer][:sample][:source]}"
-end
+  remote_file 'StandardAssets' do
+    path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:assets][:filename]}"
+    source "#{node[:unity3d][:installer][:assets][:source]}"
+  end
 
-package 'UnityEditor' do
-  source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:editor][:filename]}"
-  options "/S"
-  provider Chef::Provider::Package::Windows
-end
+  remote_file 'SampleProject' do
+    path "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:sample][:filename]}"
+    source "#{node[:unity3d][:installer][:sample][:source]}"
+  end
 
-package 'StandardAssets' do
-  source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:assets][:filename]}"
-  options "/S"
-  provider Chef::Provider::Package::Windows
-end
+  package 'UnityEditor' do
+    source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:editor][:filename]}"
+    options "/S"
+    provider Chef::Provider::Package::Windows
+  end
 
-package 'SampleProject' do
-  source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:sample][:filename]}"
-  options "/S"
-  provider Chef::Provider::Package::Windows
+  package 'StandardAssets' do
+    source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:assets][:filename]}"
+    options "/S"
+    provider Chef::Provider::Package::Windows
+  end
+
+  package 'SampleProject' do
+    source "#{Chef::Config['file_cache_path']}/#{node[:unity3d][:installer][:sample][:filename]}"
+    options "/S"
+    provider Chef::Provider::Package::Windows
+  end
+
+  ruby_block 'setInstalledMarker' do
+    block do
+      node.set[:unity3d][:is_installed] = true
+    end
+  end
 end
